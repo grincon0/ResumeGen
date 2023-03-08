d540ea47ea06a1531070f8fd073b649ba8f56c47
Heron Wake <georgerincon2@gmail.com>
2023-03-08 02:24:45 -0500

    update landing headline, added transition effects to landing elements, indexed global stylesheets

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import logo from './logo.svg';
-import './App.css';
-import JSON from './JSON/resumeContent.json'
+import JSON from './JSON/content.json'
 import Landing from './components/UI/landing/Landing';
 import ClassicLayout from './components/layouts/classic/ClassicLayout';
 import ModernLayout from './components/layouts/modern/ModernLayout';
-import './styles/_global.scss';
+import './styles/_index.scss';
+import FormWrapper from './components/UI/formWrapper/FormWrapper';
 
-const resumeValue = 1;
+
+let pageValue = 1;
 
 console.log('json app level', JSON);
 function App() {
+
+  const switchToForm = () => {
+    console.log('func called');
+    pageValue = 2;
+  }
   return (
     <>
       <head>
@@ -18,7 +24,8 @@ function App() {
         <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet" />
       </head>
         <div className="App">
-          <Landing />
+          {pageValue === 1 && <Landing propFunc={switchToForm}/>}
+          {pageValue === 2 && <FormWrapper />}
           {/* {resumeValue === 1 ? <ClassicLayout resumeData={JSON} /> : <ModernLayout resumeData={JSON} /> } */}
         </div>
     </>
--- src/components/UI/landing/Landing.jsx
+++ src/components/UI/landing/Landing.jsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.scss';
 
-const Landing = () => {
+const Landing = ({ propFunc }) => {
+  const [click, setClickState] = useState(false);
 
+  const handleButtonClick = () => {
+    if (!click) {
+      propFunc();
+      return setClickState(true);
+    }
+    setClickState(false)
+  }
+
+  console.log('click state', click);
   return (
-    <div className="c-landing">
-      <h1 className="headline">Hello</h1>
+    <div className={`c-landing ${click ? 'has-transition' : ''}`}>
+      <h1 className="headline">{`{ Hello World }`}</h1>
       <span className="sub-headline">Want to build a resume? Tired of formating layouts and subscription fees?</span>
       <span className="call-to-action">Stick it to the man and click below.</span>
-      <button>Begin Now</button>
+      <div className="c-button">
+        <button onClick={handleButtonClick}>Begin Now</button>
+      </div>
     </div>
   );
 };
