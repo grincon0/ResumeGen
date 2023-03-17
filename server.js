75a42d3ac05d98a771320f622c201b8b68fa9455
Heron Wake <georgerincon2@gmail.com>
2023-03-17 15:46:51 -0400

    setting up node

---
+++ server.js
@@ -0,0 +1,32 @@
+// Dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const path = require('path');
+//const routes = require('./routes');
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Serve up static assets (usually on heroku)
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static("client/build"));
+}
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build/index.html'), function(err) {
+      if (err) {
+        res.status(500).send(err)
+      }
+    })
+  })
+
+// Add routes, both API and View
+//app.use(routes);
+
+// Start the API server
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
