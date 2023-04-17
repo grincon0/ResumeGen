b666e50fccdab2fe25c3f672a04bbb59eafab9ae
Heron Wake <georgerincon2@gmail.com>
2023-04-17 12:05:20 -0400

    adding form handlers to Form component fro netlify

--- client/src/components/UI/form/Form.jsx
+++ client/src/components/UI/form/Form.jsx
@@ -145,29 +145,110 @@ const Form = ({ pageValue, setPageValue, isLightMode }) => {
     }
   };
 
-  const handleFormSubmit = async (event) => {
-    try {
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    setFormSubmitted(true);
+    const result = handleResumeFormData(formData);
+    if (result) setResumeData(result);
+/*     try {
       event.preventDefault();
       setFormSubmitted(true);
       const response = await axios.post('/submit-form', formData);
       setResumeData(response.data);
     } catch (error) {
       console.error(error);
-    }
+    } */
   };
 
-/*   const handleTransitionEnd = () => {
-    if (hasFormInit && isAnimating) {
-      const currentValue = formStageValue;
-      if (buttonClickType === 'back') {
-        setformStageValue(currentValue - 1);
-      } else {
-        setformStageValue(currentValue + 1);
-      }
-      setButtonClickType(null);
-      setIsAnimating(false);
+  const handleResumeFormData = (formData) => {
+
+    const buildSkillCategories = (skillArray) => {
+      if (!skillArray) return null;
+      const result = skillArray.reduce((result, entry) => {
+        const { skillRowTitle = '', skillRowString = '' } = entry;
+        return Object.assign(result, { [skillRowTitle]: skillRowString });
+      }, {});
+  
+      return result;
     }
-  }; */
+  
+    const transformedData = {
+      contact: {
+        meta: {
+          dataType: 'CONTACT',
+          hasTopBottomBorder: false
+        },
+        content: {
+          email: formData?.email,
+          name: formData?.userName,
+          title: formData?.userTitle,
+          phone: formData?.phone,
+          address: formData?.address,
+          linkedinUrl: formData?.linkedinUrl
+        }
+      },
+      companies: {
+        meta: {
+          dataType: 'WORK',
+          hasTopBottomBorder: false
+        },
+        content: formData.workEntries.map((el) => {
+          return {
+            name: el?.workName,
+            role: el?.roleTitle,
+            dateString: el?.dateString,
+            bullets: el?.workBulletString?.split('.'),
+            isContract: el?.isContractor,
+            setAgencyAsCompanyName: el?.showContractor,
+            contractor: el?.agencyName
+          }
+        })
+      },
+      projects: {
+        meta: {
+          dataType: 'PROJECT',
+          hasTopBottomBorder: false
+        },
+        links: {
+          repo: formData?.gitLink,
+          portfolio: formData?.portfolioLink
+        },
+        content: formData?.projectEntries.map((el) => {
+          return {
+            name: el?.projectName,
+            desc: el?.projectDescription,
+            divider: ' - ',
+            bullets: el?.workBulletString?.split('.')
+          }
+        })
+      },
+      education: {
+        meta: {
+          dataType: 'EDUCATION',
+          hasTopBottomBorder: false
+        },
+        content: formData?.eduEntries.map((el) => {
+          return {
+            name: el?.eduName,
+            dateString: el?.eduDateText,
+            type: el?.eduType,
+            locale: el?.eduLocale,
+            bullets: el?.workBulletString?.split('.')
+          }
+        })
+      },
+      skills: {
+        meta: {
+          dataType: 'SKILL',
+          layoutType: 'MULTI',
+          hasTopBottomBorder: false
+        },
+        categories: buildSkillCategories(formData?.skillEntries)
+      }
+    };
+  
+    return transformedData;
+  };
 
   const handleTransitionEnd = () => {
     if (!hasFormInit || !isAnimating) return;
