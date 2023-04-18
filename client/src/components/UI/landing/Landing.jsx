5dbbc20ea7b0b50659056f5c2a5a74a49ab311ce
Heron Wake <georgerincon2@gmail.com>
2023-04-18 03:17:01 -0400

    updated landing page button styling for both color modes + added semi-neumorphic box around landing page sub-headline

--- client/src/components/UI/landing/Landing.jsx
+++ client/src/components/UI/landing/Landing.jsx
@@ -15,9 +15,8 @@ const Landing = ({ propFunc }) => {
     <div className={`c-landing ${click ? 'has-transition' : ''}`}>
       <h1 onTransitionEnd={handlePropFunction} className="headline">{`{ Hello World }`}</h1>
       <span className="sub-headline">Want to build a resume? Tired of formating layouts and subscription fees?</span>
-      <span className="call-to-action">Stick it to the man and click below.</span>
       <div className="c-button">
-        <button onClick={handleButtonClick}>Begin Now</button>
+        <button onClick={handleButtonClick}>START</button>
       </div>
     </div>
   );
